d91ff0a9d4249054deee92f3171e986359beedd0
Vinicius Araujo <vadevlife@gmail.com>
2021-06-24 00:23:50 -0300

    testes

--- pages/index.js
+++ pages/index.js
@@ -11,16 +11,9 @@ import {
   import '@fortawesome/fontawesome-free/css/all.css'; 
   import 'bootstrap/dist/css/bootstrap.css'; 
   // import from 'react-bootstrap';
-  import Image from 'next/image'; 
-  import Highligts from "../components/Highligts";
-  import Footer from "../components/Footer"; 
-  import Portfolio from "../components/portfolio"; 
-  import Parallax from '../components/parallax';
-  import Download from '../components/download';
+  
   import Header from '../components/header';
-  import Skills from '../components/skills';
-  import Fullbanner from '../components/fullbanner';
-  import Benefits from '../components/benefits';
+  
 
 
  
