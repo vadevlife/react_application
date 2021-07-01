0a9182a903b80e27c6bcb8eb57fac934fd8fc50b
Vinicius Araujo <vadevlife@gmail.com>
2021-07-01 10:27:22 -0300

    Next-ga config

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,10 @@ import '../styles/app.css';
 import Router from "next/router";
 import withGA from "next-ga";
 
-
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
   
-  // Only uncomment this method if you have blocking data requirements for
-  // every single page in your application. This disables the ability to
-  // perform automatic static optimization, causing every page in your app to
-  // be server-side rendered.
-  //
-  // MyApp.getInitialProps = async (appContext) => {
-  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
-  //   const appProps = await App.getInitialProps(appContext);
-  //
-  //   return { ...appProps }
-  // }
+
   
-  // export default MyApp
-
-export default withGA("G-S53TZ2FD4T ", Router)(MyApp); 
-
-  // pass your GA code as first argument
+export default withGA("G-S53TZ2FD4T", Router)(MyApp);
