6e06a9af505215a494fac529c7af87390166064f
Vinicius Araujo <vadevlife@gmail.com>
2021-07-08 17:06:17 -0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
+          
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
