57a9b0d933aa9c8d24cfbef87e40467b527b7607
vadevlife <vadevlife@gmail.com>
2021-12-09 09:02:42 -0300

    update

---
+++ components/desk-jobs.js
@@ -0,0 +1,92 @@
+
+import React from 'React'
+import {
+    Container,
+    Row,
+    Col,
+    Card,
+    CardBody,
+    CardText,
+    CardTitle,
+    Badge,
+    List,
+    ListInlineItem    
+   } from 'reactstrap'; 
+ 
+const portfolio=[{
+    key:'1',
+    nome:'Poupatempo',
+    logo:'/poupatempo.png',
+    color:'light',
+    bg:'url("/poupatempo.png',
+    text:'Desenvolvedor Web de serviços do Portal Detran.', 
+    url:'http://www.detran.sp.gov.br/wps/portal/portaldetran/cidadao/servicos/servicosOnline',
+},{
+    key:'2',
+    nome:'Portal Detran SP',
+    logo:'/detran.png', 
+    bg:'url("/detran.png',  
+    color:'light',
+    text:'Desenvolvedor WordPress PersonalHouse / Colonial Village ||' ,
+    url:'https://colonialvillage.com.br/', 
+}]
+
+
+ 
+export default function PortfolioDesktop(){
+   return(
+       <React.Fragment>
+           
+           <Row className='p-5'>
+                    
+                    <Col sm={6} lg={4} className="p-0">
+                        
+                        <Card style={{backgroundImage:'url("/enike.png'}}> 
+                            <CardBody>
+                                <h4 className="text-white">Nike Tema E-commerce</h4>
+                                <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                            </CardBody> 
+                             
+                        </Card>     
+                        
+                    </Col>
+
+                    <Col md={6} lg={4}>
+                        <Row>
+                            <div className="col-md-12 col-lg-12 p-0" key={portfolio.key}>  
+                                    <Card className="card-centr" style={{backgroundImage:'url("/poupatempo.png'}}> 
+                                    <CardBody>
+                                        <h4 className="text-white">Portal PoupaTempo SP</h4>
+                                        <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                                    </CardBody> 
+                                    </Card> 
+                                </div>
+                                <div className="col-md-12 col-lg-12 p-0" key={portfolio.key}>  
+                                    <Card className="card-centr" style={{backgroundImage:'url("/detran.png'}}> 
+                                    <CardBody>
+                                        <h4 className="text-white">Portal Detran SP</h4>
+                                        <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                                    </CardBody> 
+                                    </Card> 
+                                </div>
+                        </Row>
+                        
+                    </Col>
+
+                   
+
+                    <Col md={4} className="p-0 d-sm-none d-lg-block">
+                        <Card style={{backgroundImage:'url("/charliekirk.png'}}> 
+                            <CardBody>
+                                <h4 className="text-white">Charlie Kirk</h4>
+                                <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                            </CardBody>  
+                             
+                        </Card>     
+                    </Col>
+                    
+                   
+                </Row>
+       </React.Fragment>
+   )
+}
--- components/portfolio.js
+++ components/portfolio.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import useMediaQuery from '/hooks/useMediaQuery'
+import PortfolioMobile from './mob-jobs'
+import PortfolioDesktop from './desk-jobs'
+
+
 import {
    Container,
    Row,
@@ -13,25 +18,11 @@ import {
   } from 'reactstrap'; 
 
 
-const portfolio=[{
-    key:'1',
-    nome:'Poupatempo',
-    logo:'/detranlogo.png',
-    color:'light',
-    bg:'url("/detran.png',
-    text:'Desenvolvedor Web de serviços do Portal Detran.', 
-    url:'http://www.detran.sp.gov.br/wps/portal/portaldetran/cidadao/servicos/servicosOnline',
-},{
-    key:'2',
-    nome:'PersonalHouse',
-    logo:'/personalhouselogo.png', 
-    bg:'url("/personalhouse.png',  
-    color:'light',
-    text:'Desenvolvedor WordPress PersonalHouse / Colonial Village ||' ,
-    url:'https://colonialvillage.com.br/', 
-}]
+
 
 export default function Portfolio(){
+    const isDesktop = useMediaQuery('(min-width: 960px)');
+    
     return(
         <React.Fragment> 
         <div id="portfolio" className='section mt-5 mb-5'>
@@ -44,83 +35,9 @@ export default function Portfolio(){
                         <h2 className="display-4">Projetos que tive o prazer de fazer parte!</h2>
                     </Col>      
                 </Row>
-                <Row className='p-5'>
-                    
-                    <Col sm={6} lg={4} className="p-0">
-                        
-                        <Card style={{backgroundImage:'url("/poupatempo.png'}}> 
-                            <CardBody>
-                                <Badge className="before-hidden rounded-pill" target="_blank" href="https://www.poupatempo.sp.gov.br/" color="light"><img src="/poupatempologo.png"/></Badge> 
-                                <CardTitle tag="h5"></CardTitle>
-                                <CardText color="muted"></CardText>
-                                <p className="text-light lead">Designer e desenvolvedor web de aplicações digitais do Portal PoupaTempo.</p> 
-                            </CardBody> 
-                            <div className="p-3">  
-                            <span className="text-black text-light">Tecnologias</span> 
-                                <List type="unstyled">
-                                    <ListInlineItem><i className="text-warning h3 fab fa-html5"></i></ListInlineItem>
-                                    <ListInlineItem><i className="text-danger h3 fab fa-css3-alt"></i></ListInlineItem>
-                                    <ListInlineItem><i className="text-primary h3 fab fa-js"></i></ListInlineItem>
-                                </List>
-                            </div>
-                        </Card>     
-                        
-                    </Col>
 
-                    <Col md={6} lg={4}>
-                        <Row>
-                            {portfolio.map((portfolio)=>( 
-            
-                                    <div className="col-md-12 col-lg-12 p-0" key={portfolio.key}>  
-                                   
-                                        <Card className="card-centr" style={{backgroundImage:portfolio.bg}}> 
-                                            <CardBody>
-                                                <Badge className="before-hidden rounded-pill" href={portfolio.url} color={portfolio.color}><img src={portfolio.logo}/></Badge> 
-                                                <CardTitle tag="h5"></CardTitle>
-                                                <CardText color="muted"></CardText>
-                                                <p className="text-white lead">{portfolio.text}</p> 
-                                            </CardBody>
-
-                                            <div className="d-flex justify-content-end sof-skills p-4 flex-column"> 
-                                                <span className="text-black text-light d-block mb-1">Tecnologias</span> 
-                                                <List type="unstyled">
-                                                    <ListInlineItem><i className="text-warning h3 fab fa-html5"></i></ListInlineItem>
-                                                    <ListInlineItem><i className="text-danger h3 fab fa-css3-alt"></i></ListInlineItem>
-                                                    <ListInlineItem><i className="text-primary h3 fab fa-js"></i></ListInlineItem>
-                                                </List>
-                                            </div>
-                                        </Card> 
-                                     
-                                    </div>
-                                    
-                                ))}
-                        </Row>
-                    </Col>
-
-                   
-
-                    <Col md={4} className="p-0 d-sm-none d-lg-block">
-                        <Card style={{backgroundImage:'url("/charliekirk.png'}}> 
-                            <CardBody>
-                                <Badge className="before-hidden rounded-pill" href="https://charliekirk.com" color="light"><img  src="/charliekirklogo.png"/></Badge> 
-                                <CardTitle tag="h5"></CardTitle>
-                                <CardText color="muted"></CardText>
-                                <p className="text-white lead">Desenvolvedor WordPress do site CharlieKirk.</p> 
-                            </CardBody> 
-                            <div className="p-3">  
-                            <span className="text-black text-light mb-1">Tecnologias</span> 
-                                <List type="unstyled">
-                                    <ListInlineItem><i className="text-warning h3 fab fa-html5"></i></ListInlineItem>
-                                    <ListInlineItem><i className="text-danger h3 fab fa-css3-alt"></i></ListInlineItem>
-                                    <ListInlineItem><i className="text-primary h3 fab fa-js"></i></ListInlineItem>
-                                    <ListInlineItem><i className="text-light h3 fab fa-wordpress"></i></ListInlineItem>
-                                </List>
-                            </div>
-                        </Card>     
-                    </Col>
-                    
-                   
-                </Row>
+                {isDesktop ?  <PortfolioDesktop/>  : <PortfolioMobile/>}
+                
                  
             </Container>
         </div>
