9c9a3c1761ddd952852eabc88d8be52e8f79d0e6
vadevlife <vadevlife@gmail.com>
2021-12-22 10:36:32 -0300

    update

--- components/full.js
+++ components/full.js
@@ -29,18 +29,16 @@ export default function full() {
                         <Card style={{backgroundImage:'url("/enike.png'}}> 
                             <CardBody>
                                 <h4 className="text-white">Loja Nike Shoes</h4>
-                                <p className="text-light lead">Aplicação para e-commerce</p> 
+                                <p className="text-light lead">Aplicação E-commerce</p> 
                               
                             </CardBody>  
                             <div className="overlay">
                                 <div className="text-content p-4">
-                                    {/* <div className="mb-3 ">Projeto</div> */}
                                     <h4 className="text-white">Tema para E-commerce</h4>
                                     <p className="text-light">Objetivo deste projeto é mostrar ao logista as possibilidades de design e recursos para sua lojas online.</p> 
                                     <span>Html5, Css3, Javascript, Node.js, automatizadores de terefas.</span>
                                     <div className="d-flex mt-3 justify-content-between">
                                         <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://vadevlife.github.io/niketheme/"><i class="fas fa-desktop mr-2"> </i>Ver Demo</a>  
-                                        {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="#">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
                                     </div>
                                 </div>       
                             </div> 
@@ -54,17 +52,15 @@ export default function full() {
                                     <Card className="card-centr" style={{backgroundImage:'url("/poupatempo.png'}}> 
                                     <CardBody>
                                         <h4 className="text-white">Portal PoupaTempo SP</h4>
-                                        <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                                        <p className="text-light lead">Web Site Responsivo</p> 
                                     </CardBody> 
                                     <div className="overlay">
                                         <div className="text-content p-4">
-                                            {/* <div className="mb-3 ">Projeto</div> */}
                                             <h4 className="text-white">Portal de serviços PoupaTempo</h4>
                                             <p className="text-light ">Portal desenvolvido para auxiliar o cidadão na retirada de documentos pessoais.</p> 
                                             <span>Html5, Css3, Javascript, Node.js, automatizadores de terefas.</span>
                                             <div className="d-flex mt-3 justify-content-between">
                                                 <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://www.poupatempo.sp.gov.br/"><i class="fas fa-desktop mr-2"> </i>Ver Site</a>  
-                                                {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href=" ">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
                                             </div>
                                         </div>       
                                     </div> 
@@ -75,7 +71,8 @@ export default function full() {
                                     <Card className="card-centr" style={{backgroundImage:'url("/detran.png'}}> 
                                         <CardBody>
                                             <h4 className="text-white">Portal Detran SP</h4>
-                                            <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                                            <p className="text-light lead">Web Site Responsivo</p> 
+
                                         </CardBody>
                                         <div className="overlay">
                                         <div className="text-content p-4">
@@ -84,7 +81,7 @@ export default function full() {
                                             <p className="text-light ">O DETRAN é o órgão responsável por realizar, fiscalizar e controlar o processo de formação de novos condutores. </p> 
                                             <span>Html5, Css3, Javascript, Node.js, automatizadores de terefas.</span>
                                             <div className="d-flex mt-3 justify-content-between">
-                                                <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://www.poupatempo.sp.gov.br/"><i class="fas fa-desktop mr-2"> </i>Ver Site</a>  
+                                                <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://www.detran.sp.gov.br/"><i class="fas fa-desktop mr-2"> </i>Ver Site</a>  
                                                 {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href=" ">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
                                             </div>
                                         </div>       
@@ -102,7 +99,7 @@ export default function full() {
                         <Card style={{backgroundImage:'url("/charliekirk.png'}}> 
                             <CardBody>
                                 <h4 className="text-white">Charlie Kirk</h4>
-                                <p className="text-light lead">Atuei no Design UI e Prototipação.</p> 
+                                <p className="text-light lead">Web Site responsivo Wordpress</p> 
                             </CardBody>  
                             <div className="overlay">
                                 <div className="text-content p-4">
@@ -162,11 +159,11 @@ export default function full() {
                                         <div className="text-content p-4">
                                             {/* <div className="mb-3 ">Projeto</div> */}
                                             <h4 className="text-white">Anúncio Whey Protein E-commerce</h4>
-                                            <p className="text-light ">Material publicitario para Redesign da Loja online Modo Saúde.</p> 
+                                            <p className="text-light ">Material publicitário para Redesign da Loja online Modo Saúde.</p> 
                                             <span>Adobe Photoshop.</span>
                                             <div className="d-flex mt-3 justify-content-between">
                                             <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="#"><i class="fas fa-image mr-2"></i>Ver Material</a>  
-                                                
+                                                 
                                                  
                                                 {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href=" ">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
                                             </div>
@@ -185,11 +182,12 @@ export default function full() {
                                         <div className="text-content p-4">
                                             {/* <div className="mb-3 ">Projeto</div> */}
                                             <h4 className="text-white">Anúncio Social Media</h4>
-                                            <p className="text-light ">O DETRAN é o órgão responsável por realizar, fiscalizar e controlar o processo de formação de novos condutores. </p> 
-                                            <span>Html5, Css3, Javascript, Node.js, automatizadores de terefas.</span>
+                                            <p className="text-light d-block text-left">Anúncio de evento para mídias sociais.</p>  
+                                            <span>Adobe Photoshop.</span>
                                             <div className="d-flex mt-3 justify-content-between">
-                                                <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://www.poupatempo.sp.gov.br/"><i class="fas fa-desktop mr-2"> </i>Ver Site</a>  
-                                                {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href=" ">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
+                                                <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="#"><i class="fas fa-image mr-2"></i>Ver Material</a>  
+                                              
+                                                 {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href=" ">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
                                             </div>
                                         </div>       
                                     </div> 
@@ -205,18 +203,18 @@ export default function full() {
                     <Col md={4} className="p-0 d-sm-none d-lg-block">
                         <Card style={{backgroundImage:'url("/dpoupa.png'}}> 
                             <CardBody>
-                                <h4 className="text-white">Portal PoupaTempo</h4>
-                                <p className="text-light lead">Design UI e Prototipação</p> 
+                                <h4 className="text-white">Design nova UI PoupaTempo</h4>
+                                <p className="text-light lead">Design UI</p> 
                             </CardBody>  
                             <div className="overlay">
                                 <div className="text-content p-4">
                                     {/* <div className="mb-3 ">Projeto</div> */}
                                     <h4 className="text-white">Kirk de Noticias</h4>
-                                    <p className="text-light">Web Site Wordpress para publicação de noticias de Charlie Kirk ativista conservador americano.</p> 
-                                    <span>WordPress, Html5, Css3, Jquery.</span>
+                                    <p className="text-light">Redesign para Portal PoupaTempo.</p> 
+                                    <span>Adobe XD.</span>
                                     <div className="d-flex mt-3 justify-content-between">
-                                        <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://charliekirk.com/"><i class="fas fa-desktop mr-2"> </i>Ver Demo</a>  
-                                        {/* <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="#">Saiba Mais<i class="fas fa-angle-right mr-2"></i> </a>   */}
+                                    <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="#"><i class="fas fa-image mr-2"></i>Ver Material</a>  
+
                                     </div>
                                 </div>       
                             </div> 
@@ -242,7 +240,7 @@ export default function full() {
                                 <div className="text-content p-4">
                                     {/* <div className="mb-3 ">Projeto</div> */}
                                     <h4 className="text-white">Loja Modo Saude</h4>
-                                    <p className="text-light">Objetivo deste projeto é mostrar ao logista as possibilidades de design e recursos para sua lojas online.</p> 
+                                    <p className="text-light">Aplicação responsiva para Modo Saude Loja de suplementos alimentares.</p> 
                                     <span>Html5, Css3, Javascript, Node.js, automatizadores de terefas.</span>
                                     <div className="d-flex mt-3 justify-content-between">
                                         <a class="btn btn-primary rounded-pill d-flex align-items-center" target="_blank" href="https://vadevlife.github.io/niketheme/"><i class="fas fa-desktop mr-2"> </i>Ver Demo</a>  
